9785c1d847d771ed9df45b75f6683c45dc79070e
mandani-tech <mandani.hamida@gmail.com>
2019-08-07 16:16:03 -0500

    completed work set4 included extra

--- index.js
+++ index.js
@@ -35,9 +35,69 @@ else if (sum < 21)
 // Ask the user to enter their grade. Print their letter grade:
 //
 //     A 90 - 100 B 80 - 89 C 70 - 79 D 60 - 69 F 0 - 59
+
+var gradeUser = parseInt(prompt("Please enter your grade here:"));
+
+ if (gradeUser > 100 || gradeUser < 0)
+{
+    alert("ERROR");
+}
+
+else if (gradeUser >= 90 && gradeUser <= 100)
+
+
+{
+    alert("You made A");
+}
+
+else if(gradeUser >=80 && gradeUser <=89)
+
+{
+    alert("You made B");
+}
+
+else if (gradeUser >= 70 && gradeUser <= 79)
+
+{
+    alert("You made C");
+
+}
+
+else if (gradeUser >= 60 && gradeUser <= 69)
+
+{
+    alert("You made D");
+
+}
+else if (gradeUser >= 0 && gradeUser <= 59)
+
+{
+    alert("You made F");
+
+}
+
+
 //
 // ##### Extra Credit:
 //     If it's negative or over 100 print an error.
+
+
+
+
 //
 // ### Exercise 12
-// Ask the user to enter their age. Check if they entered a value between 0 and 125. If valid, print "REAL AGE". If invalid print “NOT A REAL AGE!!!”
+// Ask the user to enter their age. Check if they entered a value between 0 and 125.
+// If valid, print "REAL AGE". If invalid print “NOT A REAL AGE!!!”
+
+var ageUser = parseInt(prompt("Please enter your age here:"));
+if (ageUser >= 0 && ageUser <= 125)
+
+{
+
+    alert("REAL AGE");
+
+}
+
+else
+
+    alert("Not A Real Age");
